f93f0f32b552482927dfde461ca3902705073a91
yossi <yossi@delver.com>
2013-03-03 17:56:02 +0200

    Adding tests

---
+++ net/src/MvcDemo/MvcDemo.Tests/HomeControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using MvcDemo.Controllers;
+using NUnit.Framework;
+
+namespace MvcDemo.Tests
+{
+	[TestFixture]
+	public class HomeControllerTests
+    {
+		private HomeController _target;
+
+		[SetUp]
+		public void Setup()
+		{
+			_target = new HomeController();
+		}
+		
+
+		[Test]
+		public void Index_WhenCalled_ReturnsMessage()
+		{
+			var result = _target.Index();
+
+			Assert.That(result, Is.Not.Null);
+		}
+    }
+}
